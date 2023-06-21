feb4caec362ca4a5fc629725cc7bf88f48aec305
Joel0919 <joel09llalli@gmail.com>
2023-06-21 18:38:59 -0500

    Realizando la validación y envio de mensaje por email

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,37 +1,45 @@
-// document.querySelector(".toggle input").addEventListener("change", (e) => {
-//   document.body.classList.toggle("dark");
-// });
- 
+$(window, document, undefined).ready(function () {
+  $(".contacto__form-input").blur(function () {
+    var $this = $(this);
+    if ($this.val()) $this.addClass("used");
+    else $this.removeClass("used");
+  });
+});
+(() => {
+  // document.querySelector(".toggle input").addEventListener("change", (e) => {
+  //   document.body.classList.toggle("dark");
+  // });
 
-//identify the toggle switch HTML element
-const toggleSwitch = document.querySelector('#checkbox_theme');
+  //identify the toggle switch HTML element
+  const toggleSwitch = document.querySelector("#checkbox_theme");
 
-//function that changes the theme, and sets a localStorage variable to track the theme between page loads
-function switchTheme(e) {
+  //function that changes the theme, and sets a localStorage variable to track the theme between page loads
+  const switchTheme = (e) => {
     if (e.target.checked) {
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', 'dark');
-        toggleSwitch.checked = true;
-        document.getElementById("toggle").classList.add("dark");
-        document.getElementById("toggle__btn").classList.add("dark");
-        // document.getElementById("header__logo").src="assets/img/logo2.png";
-      } else {
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', 'light');
-        toggleSwitch.checked = false;
-        document.getElementById("toggle").classList.remove("dark");
-        document.getElementById("toggle__btn").classList.remove("dark");
-        // document.getElementById("header__logo").src="assets/img/logo1.png";
-    }    
-}
+      localStorage.setItem("theme", "dark");
+      document.documentElement.setAttribute("data-theme", "dark");
+      toggleSwitch.checked = true;
+      document.getElementById("toggle").classList.add("dark");
+      document.getElementById("toggle__btn").classList.add("dark");
+      // document.getElementById("header__logo").src="assets/img/logo2.png";
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.setAttribute("data-theme", "light");
+      toggleSwitch.checked = false;
+      document.getElementById("toggle").classList.remove("dark");
+      document.getElementById("toggle__btn").classList.remove("dark");
+      // document.getElementById("header__logo").src="assets/img/logo1.png";
+    }
+  };
 
-//listener for changing themes
-toggleSwitch.addEventListener('change', switchTheme, false);
+  //listener for changing themes
+  toggleSwitch.addEventListener("change", switchTheme, false);
 
-//pre-check the dark-theme checkbox if dark-theme is set
-if (document.documentElement.getAttribute("data-theme") == "dark"){
+  //pre-check the dark-theme checkbox if dark-theme is set
+  if (document.documentElement.getAttribute("data-theme") == "dark") {
     toggleSwitch.checked = true;
     document.getElementById("toggle").classList.add("dark");
     document.getElementById("toggle__btn").classList.add("dark");
-    document.getElementById("img1").src="assets/img/logo2.png";
-}
+    document.getElementById("img1").src = "assets/img/logo2.png";
+  }
+})();
